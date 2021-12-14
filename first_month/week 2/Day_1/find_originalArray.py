5ced16ba492f959d0b03b7b1af1d139547f4914d
keiredin <se.keiredin.aman@gmail.com>
2021-12-14 16:44:46 +0300

    leetcode: find the original array from doubled array

---
+++ first_month/week 2/Day_1/find_originalArray.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        original = []
+        n = len(changed)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if(changed[j] == 2 * changed[i]):
+                    original.append(changed[i])
+                    changed[j] = -1         
+        return original
+
+s = Solution()
+print(s.findOriginalArray([1,3,4,2,6,8]))
+print(s.findOriginalArray([6,3,0,1]))
+print(s.findOriginalArray([1]))
+print(s.findOriginalArray([0]))
+
