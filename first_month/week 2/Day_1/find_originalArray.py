6a4b8552e1cfad76c66ff544277ff4c49ebbb599
keiredin <se.keiredin.aman@gmail.com>
2021-12-15 12:39:30 +0300

    time complexity optimized to be = nlogn

--- first_month/week 2/Day_1/find_originalArray.py
+++ first_month/week 2/Day_1/find_originalArray.py
@@ -1,26 +1,28 @@
-from typing import List
-
+from typing import Counter, List
 
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        original = []
         n = len(changed)
+        if(n%2 != 0):
+            return []
+        count = Counter(changed)
+        changed.sort()
+        original = []
         
-        if(n % 2 == 0 ):
-            for i in range(n-1):
-                for j in range(i+1,n):
-                    if(changed[j] == 2 * changed[i]):
-                        original.append(changed[i])
-                        changed[j] = -1
-                        
-        else:
-            original = []
-
-        return original
+        for num in changed:
+            if num == 0 and count[num] >= 2:
+                count[num] -= 2
+                original.append(num)
+            elif num > 0 and count[num] and count[num*2]:
+                count[num] -= 1
+                count[num*2] -= 1
+                original.append(num)
+                
+        return original if len(original) == n // 2 else []
 
 s = Solution()
 print(s.findOriginalArray([1,3,4,2,6,8]))
 print(s.findOriginalArray([6,3,0,1]))
 print(s.findOriginalArray([1]))
-print(s.findOriginalArray([0]))
-
+print(s.findOriginalArray([0,2,0,4]))
+print(s.findOriginalArray([0,0,0,0]))
