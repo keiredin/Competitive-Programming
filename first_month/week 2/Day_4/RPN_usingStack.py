1c13c5e3a9c4e660ce26531c64aedbadd89941ef
keiredin <se.keiredin.aman@gmail.com>
2021-12-16 22:36:23 -0600

    leetcode: evaluate reverse polish notation using stack

---
+++ first_month/week 2/Day_4/RPN_usingStack.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        s = []
+        o = "+-/*"
+        for i in tokens:
+            if i in o: 
+                num1 = s.pop()
+                num2 = s.pop()
+                if i == '+':
+                    num2 += num1 
+                    s.append(num2)
+                elif i == '-':
+                    num2 -= num1
+                    s.append(num2)
+                    
+                elif i == '*':
+                    num2 *= num1
+                    s.append(num2)
+                    
+                elif i == '/':
+                    if (num2 * num1 > 0):
+                        num2 = num2 // num1
+                    else:
+                        num2 = (num2+(-num2 % num1)) //num1  
+                    s.append(num2)
+            else:
+                s.append(int(i))
+                
+                    
+        return s.pop()
+
+s = Solution()
+print(s.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
