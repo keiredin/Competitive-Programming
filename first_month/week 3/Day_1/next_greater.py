6d8ca7bad556d3fc8dc296da1cc9fc0fc6e1c381
keiredin <se.keiredin.aman@gmail.com>
2021-12-20 10:55:28 -0600

    leetcode: monotonic stack - next greater Element

---
+++ first_month/week 3/Day_1/next_greater.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n1 = len(nums1)
+        n2 = len(nums2)
+        stack = []
+        dict = {}
+        
+        for i in range(n2):
+            if not stack:
+                stack.append(nums2[i])
+            else:
+                while stack and stack[-1] < nums2[i]:
+                    pop = stack.pop()
+                    dict[pop] = nums2[i]
+                    
+                stack.append(nums2[i])
+                
+        answer = []
+        for i in range(n1):
+            if nums1[i] not in dict:
+                dict[nums1[i]] = -1
+            answer.append(dict[nums1[i]])
+            
+        return answer
