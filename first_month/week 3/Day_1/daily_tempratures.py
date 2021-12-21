b364a861888f8e9c94b2cd38939da20b507ee73d
keiredin <se.keiredin.aman@gmail.com>
2021-12-21 09:26:10 -0600

    passed all test cases

--- first_month/week 3/Day_1/daily_tempratures.py
+++ first_month/week 3/Day_1/daily_tempratures.py
@@ -1,25 +1,27 @@
 from typing import List
 
-
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
         stack = []
         answer = [0] * n
+        dict = {}
         
         for i in range(n):
             if not stack:
-                stack.append(temperatures[i])
+                stack.append((temperatures[i], i))
             else:
-                while stack and stack[-1] < temperatures[i]:
-                    pop = stack.pop()
-                    popIndex = temperatures.index(pop)
-                    answer[popIndex] = i - popIndex
-                    
-                stack.append(temperatures[i])
+                while stack and stack[-1][0] < temperatures[i]:
+                    pop, idx = stack.pop()
+                    answer[idx] = i - idx
+                stack.append((temperatures[i],i))
         
         return answer
 
 s = Solution()
 
 print(s.dailyTemperatures([73,74,75,71,69,72,76,73]))
+
+
+
+
